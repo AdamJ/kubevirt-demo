7185cb9545c4fdf13735b5e4808b678c2da623e0
Adam Jolicoeur <adam.jolicoeur@redhat.com>
2018-07-11 19:44:52 -0400

    update

--- js/kubevirt-io.js
+++ js/kubevirt-io.js
@@ -1,10 +1,10 @@
 // jQuery to collapse the navbar on scroll
 function collapseNavbar() {
-    if ($(".navbar").offset().top > 50) {
-        $(".fixed-top").addClass("scroll-collapse");
-    } else {
-        $(".fixed-top").removeClass("scroll-collapse");
-    }
+  if ($(".navbar").offset().top > 50) {
+    $(".fixed-top").addClass("scroll-collapse");
+  } else {
+    $(".fixed-top").removeClass("scroll-collapse");
+  }
 }
 
 $(window).scroll(collapseNavbar);
